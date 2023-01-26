9a240145c11bb6c1a3e30526d6d63b6204f112e5
Jennifer Herrarte <36706323+jennherrarte@users.noreply.github.com>
2023-01-26 11:12:36 -0800

    update

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+
+//GET ENV VARIABLES FROM
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql'
+    });
+
+module.exports = sequelize;
+
+
+
+
